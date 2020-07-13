599892a06295442bf60be72a16669181c0afb484
Larissa Santos Azevedo <larissa.santosazevedo@hotmail.com>
2020-07-12 23:15:28 -0300

    create calculator structure

---
+++ react-hooks-jest-tests/src/calculadora.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './calculadora.css';
+import { Jumbotron } from 'react-bootstrap';
+
+function Calculadora() {
+  return (
+    <Jumbotron style={{
+      background: 'transparent !important',
+      backgroundColor: '#007bff',
+      padding: '5px',
+      margin: '5px',
+      width: '400px',
+    }}>
+
+    </Jumbotron>
+  );
+}
+
+export default Calculadora;
