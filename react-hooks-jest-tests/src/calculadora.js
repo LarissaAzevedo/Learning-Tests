5e2f35534fccafd89b53cb108328a462e7b7f754
Larissa Santos Azevedo <larissa.santosazevedo@hotmail.com>
2020-07-13 21:16:48 -0300

    finish interface

--- react-hooks-jest-tests/src/calculadora.js
+++ react-hooks-jest-tests/src/calculadora.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './calculadora.css';
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, Container, Row, Col, Button, Form } from 'react-bootstrap';
 
 function Calculadora() {
   return (
@@ -11,6 +11,76 @@ function Calculadora() {
       margin: '5px',
       width: '400px',
     }}>
+      <Container>
+        <Row>
+          <Col xs="3">
+            <Button variant="danger">C</Button>
+          </Col>
+          <Col xs="9">
+            <Form.Control type="text" name="txtNumbers" class="text-rigth" readOnly="readOnly" />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button variant="light">7</Button>
+          </Col>
+          <Col>
+            <Button variant="light">8</Button>
+          </Col>
+          <Col>
+            <Button variant="light">9</Button>
+          </Col>
+          <Col>
+            <Button variant="warning">/</Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button variant="light">4</Button>
+          </Col>
+          <Col>
+            <Button variant="light">5</Button>
+          </Col>
+          <Col>
+            <Button variant="light">6</Button>
+          </Col>
+          <Col>
+            <Button variant="warning">*</Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button variant="light">1</Button>
+          </Col>
+          <Col>
+            <Button variant="light">2</Button>
+          </Col>
+          <Col>
+            <Button variant="light">3</Button>
+          </Col>
+          <Col>
+            <Button variant="warning">-</Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button variant="light">0</Button>
+          </Col>
+          <Col>
+            <Button variant="light">.</Button>
+          </Col>
+          <Col>
+            <Button variant="success">=</Button>
+          </Col>
+          <Col>
+            <Button variant="warning">+</Button>
+          </Col>
+        </Row>
+      </Container>
 
     </Jumbotron>
   );
